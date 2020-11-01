3ec3be1cd6683e569d04868f90dd870b34d3dbba
winterbeak <winterbeak@gmail.com>
2020-11-01 12:09:34 -0500

    Feature: Add a punchable entity for testing
    Testing that I can simply extend PunchableGravityCollision in order to
    make a punchable object.

--- entities/test.py
+++ entities/test.py
@@ -30,3 +30,35 @@ class TopDown(collision.Collision):
             self.y_vel -= 5
         if self.down_key.is_held:
             self.y_vel += 5
+
+
+class Punchable(collision.PunchableGravityCollision):
+    left_key = events.Keybind([pygame.K_a, pygame.K_LEFT])
+    right_key = events.Keybind([pygame.K_d, pygame.K_RIGHT])
+    up_key = events.Keybind([pygame.K_w, pygame.K_UP])
+    down_key = events.Keybind([pygame.K_s, pygame.K_DOWN])
+
+    def __init__(self, level, camera, width, height, x, y):
+        super().__init__(level, width, height, 20, x, y)
+        self.camera = camera
+        self.collide_void = False
+
+    def update(self):
+        self._take_inputs()
+        super().update()
+
+    def _take_inputs(self):
+        if self.left_key.is_held:
+            if self.right_key.is_held:
+                self.x_vel = 0
+            else:
+                self.x_vel = -5
+        elif self.right_key.is_held:
+            self.x_vel = 5
+        else:
+            self.x_vel = 0
+
+        if self.up_key.is_held:
+            self.y_vel = -5
+        if self.down_key.is_held:
+            self.y_vel = 5
